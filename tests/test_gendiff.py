a55c4fb7bd4ff06d30b7272aecf730f9aa9c443f
Aleksey Chudinov <alekseychudinov@gmail.com>
2022-07-26 23:04:43 +0500

    Error correction_2

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -7,11 +7,6 @@ def test_generate_diff():
     diff_yml = generate_diff("tests/fixtures/file1.yml", "tests/fixtures/file2.yml", 'json')
     result_true_json = open("tests/fixtures/result_true_json.json").read()
 
-    diff_json6 = generate_diff("tests/fixtures/file61.json", "tests/fixtures/file62.json", 'json')
-    diff_yaml6 = generate_diff("tests/fixtures/file61.yaml", "tests/fixtures/file62.yaml", 'json')
-    diff_yml6 = generate_diff("tests/fixtures/file61.yml", "tests/fixtures/file62.yml", 'json')
-    result_true_json6 = open("tests/fixtures/result_true_json6.json").read()
-
     result_true_plain = open("tests/fixtures/result_true_plain.txt").read()
     diff_plain = generate_diff("tests/fixtures/file61.yml", "tests/fixtures/file62.yml", 'plain')
 
@@ -19,12 +14,3 @@ def test_generate_diff():
     assert diff_json == result_true_json
     assert diff_yaml == result_true_json
     assert diff_yml == result_true_json
-    assert diff_json6 == result_true_json6
-    assert diff_yaml6 == result_true_json6
-    assert diff_yml6 == result_true_json6
-
-
-def test_generate_diff8():
-    result_true_json8 = open("tests/fixtures/result_true_json8.json").read()
-    diff_json8 = generate_diff("tests/fixtures/file81.json", "tests/fixtures/file82.json", 'json')
-    assert diff_json8 == result_true_json8

2c1fee2e29a73a1a8f4a61ce1f1a77f8f2616702
Aleksey Chudinov <alekseychudinov@gmail.com>
2022-07-27 22:36:56 +0500

    Error correction_2

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,16 +1,34 @@
 from gendiff.generator_diff import generate_diff
 
 
-def test_generate_diff():
-    diff_json = generate_diff("tests/fixtures/file1.json", "tests/fixtures/file2.json", 'json')
-    diff_yaml = generate_diff("tests/fixtures/file1.yaml", "tests/fixtures/file2.yaml", 'json')
-    diff_yml = generate_diff("tests/fixtures/file1.yml", "tests/fixtures/file2.yml", 'json')
+def test_generate_json():
+    diff_json_json = generate_diff("tests/fixtures/file1.json", "tests/fixtures/file2.json", 'json')
+    diff_yaml_json = generate_diff("tests/fixtures/file1.yaml", "tests/fixtures/file2.yaml", 'json')
+    diff_yml_json = generate_diff("tests/fixtures/file1.yml", "tests/fixtures/file2.yml", 'json')
     result_true_json = open("tests/fixtures/result_true_json.json").read()
 
-    result_true_plain = open("tests/fixtures/result_true_plain.txt").read()
-    diff_plain = generate_diff("tests/fixtures/file61.yml", "tests/fixtures/file62.yml", 'plain')
+    assert diff_json_json == result_true_json
+    assert diff_yaml_json == result_true_json
+    assert diff_yml_json == result_true_json
 
-    assert diff_plain == result_true_plain
-    assert diff_json == result_true_json
-    assert diff_yaml == result_true_json
-    assert diff_yml == result_true_json
+
+def test_generate_plain():
+    diff_json_plain = generate_diff("tests/fixtures/file1.json", "tests/fixtures/file2.json", 'plain')
+    diff_yaml_plain = generate_diff("tests/fixtures/file1.yaml", "tests/fixtures/file2.yaml", 'plain')
+    diff_yml_plain = generate_diff("tests/fixtures/file1.yml", "tests/fixtures/file2.yml", 'plain')
+    result_true_plain = open("tests/fixtures/result_true_plain.txt").read()
+
+    assert diff_json_plain == result_true_plain
+    assert diff_yaml_plain == result_true_plain
+    assert diff_yml_plain == result_true_plain
+
+
+def test_generate_stylish():
+    diff_json_stylish = generate_diff("tests/fixtures/file1.json", "tests/fixtures/file2.json", 'stylish')
+    diff_yaml_stylish = generate_diff("tests/fixtures/file1.yaml", "tests/fixtures/file2.yaml", 'stylish')
+    diff_yml_stylish = generate_diff("tests/fixtures/file1.yml", "tests/fixtures/file2.yml", 'stylish')
+    result_true_stylish = open("tests/fixtures/result_true_stylish.txt").read()
+
+    assert diff_json_stylish == result_true_stylish
+    assert diff_yaml_stylish == result_true_stylish
+    assert diff_yml_stylish == result_true_stylish

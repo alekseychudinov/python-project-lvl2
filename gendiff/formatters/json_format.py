412250be12e975838cca1cf0e909ef89107c84f8
Aleksey Chudinov <alekseychudinov@gmail.com>
2022-07-28 20:36:24 +0500

    Error correction_2

--- gendiff/formatters/json_format.py
+++ gendiff/formatters/json_format.py
@@ -23,11 +23,11 @@ def make_sub_string(sub_list, sub_string, depth):
                 make_sub_string(list_of_children, sub_string, depth + 1) + "]\n"
             )
         elif type_of_property in ("deleted", "added", "unchanged"):
-            sub_string = f'{sub_string}{build_indent(depth)}"value": {map_value(get_value(item_dict), depth)}\n' # noqa
+            sub_string = f'{sub_string}{build_indent(depth)}"value":{map_value(get_value(item_dict), depth)}\n' # noqa
         elif type_of_property == "updated":
             sub_string = (
-                f'{sub_string}{build_indent(depth)}"value1": {map_value(get_value(item_dict)[0], depth)},\n' # noqa
-                f'{build_indent(depth)}"value2": {map_value(get_value(item_dict)[1], depth)}\n' # noqa
+                f'{sub_string}{build_indent(depth)}"value1":{map_value(get_value(item_dict)[0], depth)},\n' # noqa
+                f'{build_indent(depth)}"value2":{map_value(get_value(item_dict)[1], depth)}\n' # noqa
             )
         sub_string = f"{sub_string}{build_indent(depth - 1)}}}"
         if index < len(sub_list) - 1:
@@ -40,15 +40,15 @@ def map_value(key_value, depth):
         indent = f"\n{build_indent(depth)}{{\n"
         return f'{map_dict_value(key_value, indent, depth + 1)}{"    " * depth}}}' # noqa
     elif str(key_value) == "True":
-        return "true"
+        return " true"
     elif str(key_value) == "False":
-        return "false"
+        return " false"
     elif str(key_value) == "None":
-        return "null"
+        return " null"
     elif isinstance(key_value, int):
-        return str(key_value)
+        return f' {str(key_value)}'
     else:
-        return f'"{str(key_value)}"'
+        return f' "{str(key_value)}"'
 
 
 def map_dict_value(value_dict, sub_string, any_depth):
@@ -58,7 +58,7 @@ def map_dict_value(value_dict, sub_string, any_depth):
             sub_string = f"{sub_string}\n" f"{build_indent(any_depth)}{{\n"
             sub_string = f"{map_dict_value(value_dict[key], sub_string, any_depth + 1)}{build_indent(any_depth)}}}" # noqa
         else:
-            sub_string = f"{sub_string} {map_value(value_dict[key], any_depth)}"
+            sub_string = f"{sub_string}{map_value(value_dict[key], any_depth)}"
         if i < len(value_dict) - 1:
             sub_string += ","
         sub_string += "\n"
